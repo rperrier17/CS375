d26c5a1f16a4c723cbbc9c4d39b7bf6266d577f8
rperrier17 <rperrier17@my.whitworth.edu>
2016-04-10 14:11:37 -0700

    More work to connect to Azure

--- Battleship/app/src/main/java/com/gavettperrier/battleship/MainActivity.java
+++ Battleship/app/src/main/java/com/gavettperrier/battleship/MainActivity.java
@@ -1,8 +1,10 @@
 package com.gavettperrier.battleship;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,12 +15,20 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Test Parse
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo","bar");
+        testObject.saveInBackground();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -81,4 +91,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ Battleship/app/src/main/java/com/gavettperrier/battleship/SetupApplication.java
@@ -0,0 +1,20 @@
+package com.gavettperrier.battleship;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by Rachel on 4/10/2016.
+ */
+public class SetupApplication extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(getBaseContext())
+                        .applicationId("f1b626b8-cbb4-4dc7-b973-e98b2fa4de02")
+                        .server("http://cs375bship.azurewebsites.net/parse")
+                        .build()
+        );
+    }
+}
