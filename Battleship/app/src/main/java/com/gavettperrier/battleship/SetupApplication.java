e569b6b183cbe0948f9c9068490b42e917a46170
rperrier17 <rperrier17@my.whitworth.edu>
2016-04-18 12:53:51 -0700

    Fix parse url
    Possibly make https:// or at / at end of parse in url

--- Battleship/app/src/main/java/com/gavettperrier/battleship/SetupApplication.java
+++ Battleship/app/src/main/java/com/gavettperrier/battleship/SetupApplication.java
@@ -15,7 +15,7 @@ public class SetupApplication extends Application {
        Parse.enableLocalDatastore(this);
        Parse.initialize(new Parse.Configuration.Builder(getBaseContext())
                 .applicationId("f1b626b8-cbb4-4dc7-b973-e98b2fa4de02")
-                .server("http://cs375bship.azurewebsites.net/parse")
+                .server("https://cs375bship.azurewebsites.net/parse")//put / at the end of the url
                        .clientKey(" ")
                 .build()
         );
